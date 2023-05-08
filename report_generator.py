fa0ddf3309659c8395cbcf6f4c1c1b755118d625
maria-mz <132791559+maria-mz@users.noreply.github.com>
2023-05-08 11:40:30 -0700

    Generate complete time sheet from database

--- report_generator.py
+++ report_generator.py
@@ -1,49 +1,24 @@
 from pymongo import MongoClient
+from reportlab.lib.pagesizes import A4
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.platypus import Table
+from reportlab.lib import colors
 
 client = MongoClient('mongodb://localhost:27017/')
 database = client['employee_data']
 employees = database['employees']
 
-from reportlab.lib.pagesizes import A4
-from reportlab.pdfgen import canvas
-from reportlab.lib.units import mm
-from reportlab.lib.units import inch
-
 PDF_WIDTH, PDF_HEIGHT = A4
 
-def draw_grid(pdf):
-    pdf.setStrokeAlpha(0.25)
-
-    # draw horizontal center line
-    pdf.line(0, PDF_HEIGHT/2, PDF_WIDTH, PDF_HEIGHT/2)
-
-    # draw vertical center line
-    pdf.line(PDF_WIDTH/2, 0, PDF_WIDTH/2, PDF_HEIGHT)
-
-    # draw grid lines
-    pdf.setStrokeColorRGB(0, 0, 1) 
-    pdf.setLineWidth(0.25)
-
-    # draw horizontal grid lines
-    for y in range(int(PDF_HEIGHT/2), int(PDF_HEIGHT), int(10*mm)):
-        pdf.line(0, y, PDF_WIDTH, y)
-        pdf.line(0, PDF_HEIGHT-y, PDF_WIDTH, PDF_HEIGHT-y)
-
-    # draw vertical grid lines
-    for x in range(int(PDF_WIDTH/2), int(PDF_WIDTH), int(10*mm)):
-        pdf.line(x, 0, x, PDF_HEIGHT)
-        pdf.line(PDF_WIDTH-x, 0, PDF_WIDTH-x, PDF_HEIGHT)
-
-
-def write_text(font_size, text_content, x, y):
+def write_text(font, font_size, text_content, x, y):
 	text = pdf.beginText()
-	text.setFont('Helvetica', font_size)
+	text.setFont(font, font_size)
 	text.setTextOrigin(x, y)
 	text.textLine(text_content)
 	pdf.drawText(text)
 
-
-def draw_header(pdf):
+def draw_header():
 	pdf.setStrokeColorRGB(0, 0, 0)
 
 	top_rect_width = PDF_WIDTH / 3.5
@@ -77,11 +52,113 @@ def draw_header(pdf):
 	pdf.line(bottom_rect_x + bottom_rect_width, bottom_rect_y, bottom_rect_x + bottom_rect_width, bottom_rect_y + bottom_rect_height)
 
 	# Add the titles
-	write_text(14, "BRIGHT BYTE INC.", top_rect_x + 20, top_rect_y + top_rect_height - 20)
-	write_text(12, "CREW TIMESHEET", bottom_rect_x + 10, bottom_rect_y + bottom_rect_height - 15)
+	write_text('Helvetica', 14, "BRIGHT BYTE INC.", top_rect_x + 20, top_rect_y + top_rect_height - 20)
+	write_text('Helvetica', 12, "CREW TIMESHEET", bottom_rect_x + 10, bottom_rect_y + bottom_rect_height - 15)
+
+
+def extract_data(employee_data, table_data):
+	total_reg = 0
+	total_ot = 0
+	week_ending = ""
+
+	for row in employee_data:
+		total_reg += row['regular_hours']
+		total_ot += row['overtime_hours']
+		table_data.append([
+			row['day_of_week'][:3],
+    		row['work_date'],
+			row['time_in'],
+			row['time_out'],
+			format(row['regular_hours'], '.2f'),
+			format(row['overtime_hours'], '.2f')
+		])
+		if row['day_of_week'] == "Friday":
+			week_ending = row['work_date']
+	
+	return total_reg, total_ot, week_ending
+
+
+def write_document():
+	table_data = []
+	# Add the two headers for the table
+	table_data.append(["Day", "Date", "Time", "", "Hours"])		
+	table_data.append(["", "", "In", "Out", "Reg.", "OT"])		
+
+	# Loop through all employees
+	employee_cursor = employees.find({})
+	for employee in employee_cursor:
+		name = employee["full_name"]
+		job = employee["job_title"]
+		data = employee["work_days"]
+		
+		write_employee_name(name)
+		write_job(job)
+
+		first_week_data = data[:len(data)//2]
+		second_week_data = data[len(data)//2:]
+
+		week_data_list = [(first_week_data, True), (second_week_data, False)]
+
+		# Loop through the week data
+		for week_data, is_first in week_data_list:
+			total_reg, total_ot, week_ending = extract_data(week_data, table_data)
+			table_data.append(["", "", "", "Total:", format(total_reg, '.2f'), format(total_ot, '.2f')])
+			write_week_ending(week_ending, is_first)
+			draw_table(table_data, is_first)
+			table_data = table_data[:2]
+
+		# Move to next page
+		pdf.showPage()
+
+
+def write_employee_name(name):
+	y = PDF_HEIGHT - (PDF_HEIGHT / 5)
+	write_text('Helvetica', 12, "Employee", inch, y)
+	write_text('Helvetica', 12, name, inch * 3, y)
+
+
+def write_job(job):
+	y = PDF_HEIGHT - (PDF_HEIGHT / 5) - 30
+	write_text('Helvetica', 12, "Job Title", inch, y)
+	write_text('Helvetica', 12, job, inch * 3, y)
+
+
+def write_week_ending(date, is_first):
+	y = PDF_HEIGHT - (PDF_HEIGHT / 5) - 60
+	write_text('Helvetica-Bold', 12, "Week Ending", inch, y if is_first else y - 250)
+	write_text('Helvetica', 12, date, inch * 3, y if is_first else y - 250)
         
+
+def draw_table(data, is_first):
+	table = Table(data)
+
+	# Add style to table cells
+	table.setStyle([
+		('SPAN', (2, 0), (3, 0)),		# "Time" spans two columns
+		('SPAN', (4, 0), (5, 0)),		# "Hours" spans two columns
+		('GRID', (0, 0), (-1, 0), 1, colors.black),
+		('GRID', (2, 1), (-1, 1), 1, colors.black),
+		('GRID', (3, 9), (-1, -1), 1, colors.black),
+		('GRID', (0, 2), (-1, -2), 1, colors.black),
+		('FONTSIZE', (0, 0), (-1, -1), 12),
+		('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+		('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+		('LEFTPADDING', (0, 0), (1, 1), 24),
+		('RIGHTPADDING', (0, 0), (1, 1), 24),
+		('BOTTOMPADDING', (0, 0), (-1, -1), 4),
+		('LEFTPADDING', (2, 2), (-1, -1), 12),
+		('RIGHTPADDING', (2, 2), (-1,- 1), 12),
+		('BOX', (0, 1), (1, 2), 1, colors.black),
+	])
+
+	# Draw the table on the canvas
+	table.wrapOn(pdf, 0, 0)
+	table.drawOn(pdf, inch*1.5, 400 if is_first else 150)
+
     
 pdf = canvas.Canvas("test.pdf", pagesize=A4)
-draw_grid(pdf)
-draw_header(pdf)
+
+draw_header()
+write_document()
+
 pdf.save()
