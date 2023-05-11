fcf847d4b9d152e8058abc83b425dfaafaa84970
maria-mz <132791559+maria-mz@users.noreply.github.com>
2023-05-11 11:23:23 -0700

    Add new methods for DatabaseHandler class
    This commit added a new collection that tracks pay period start dates. Methods for getting and setting start dates were added. Also improved/refactored old methods.

--- database_handler.py
+++ database_handler.py
@@ -6,38 +6,62 @@ from pymongo import MongoClient
 NUM_PAY_PERIOD_DAYS = 14
 DEFAULT_REG = 8.00
 DEFAULT_OT = 0.00
-TIME_IN = "9:00 AM"
-TIME_OUT = "5:00 PM"
-
-# Delete later
-year = 2023
-month = 1
-day = 9
+TIME_IN = '9:00 AM'
+TIME_OUT = '5:00 PM'
 
 class DatabaseHandler:
     def __init__(self):
         client = MongoClient('mongodb://localhost:27017/')
 
-        # Access the database and collection. If doesn't exist, will be created upon first write.
+        # Access the database and collections. If doesn't exist, will be created upon first write.
         database = client['employee_data']
         self.employees = database['employees']
+        self.start_date = database['start_date']
     
     def read_csv(self):
+        """
+        Reads data from a CSV file and initializes the employee collection with the data. The method expects a start date to have been set using "set_start_date" method. Each row in the CSV file is expected to contain employee data in the following order: full name, job title. 
+
+        Returns:
+            int: 0 on success, 1 on failure to find a start date
+        """
+        if not self.has_start_date():
+            return 1
+        
+        self.employees.delete_many({})
+        year, month, day = self.get_start_date()
+        default_work_days = self._initialize_work_days(year, month, day)
+        employee_list = []
+
         with open('test_data.csv') as csvfile:
             reader = csv.reader(csvfile)
             # Skip the first row (header), handle options later
             next(reader)
+            
             for row in reader:
                 employee_data = {
                     "full_name": row[0],
                     "job_title": row[1],
-                    "work_days": self.initialize_work_days(year, month, day)
+                    "work_days": default_work_days
                 }
-                self.employees.insert_one(employee_data)
+                employee_list.append(employee_data)
+            
+        if employee_list:
+            self.employees.insert_many(employee_list)
+
+        return 0
         
     def _initialize_work_days(self, year, month, day):
         """
-        Returns a list of of work days for a given pay period.
+        Populates a list with default data for each work day in the pay period
+
+        Parameters:
+            year (int): the year of the start date.
+            month (int): the month of the start date.
+            day (int): the day of the start date.
+
+        Returns:
+            A list of dictionaries, where each dictionary represents a work day
         """
         work_days = []
 
@@ -46,7 +70,7 @@ class DatabaseHandler:
         for i in range(NUM_PAY_PERIOD_DAYS):
             date = date_obj.strftime('%m/%d/%Y')
             day_of_week = calendar.day_name[date_obj.weekday()]
-            is_weekend = day_of_week == "Saturday" or day_of_week == "Sunday"
+            is_weekend = day_of_week == calendar.SATURDAY or day_of_week == calendar.SUNDAY
             work_day = {
                 "work_date": date,
                 "day_of_week": day_of_week,
@@ -61,19 +85,69 @@ class DatabaseHandler:
             
         return work_days
     
-    def get_data(self):
+    def get_all_employees(self):
         """
-        Returns a list that contains each employee's information
-        """
-        data = []
-        for document in self.employees.find():
-            data.append(document)
-        return data
-    
-    def num_entries(self):
-        return self.employees.count_documents({})
-    
+        Retrieve all employee data from the database.
 
+        Returns:
+            A list of dictionaries, where each dictionary represents one employee record
+            in the database.
+        """
+        return list(self.employees.find())
+    
+    def get_employee(self, name):
+        """
+        Retrive an employee's information
+
+        Parameters:
+            name (str): The full name of the employee
+
+        Returns:
+            A single dictionary representing the employee record matching the provided name.
+            None if no match is found
+        """
+        return self.employees.find_one({"full_name": name})
+    
+    def set_date_and_reset(self, year, month, day):
+        """
+        Sets the start date for the payroll system. 
+        Updates the employee data to reflect the new pay period
+
+        Parameters:
+            year (int): the year of the start date.
+            month (int): the month of the start date.
+            day (int): the day of the start date.
+        """
+        self.start_date.delete_many({})
+
+        date = {
+            "year": year,
+            "month": month,
+            "day": day
+        }
+
+        self.start_date.insert_one(date)
+        self._reset_employee_data()
+
+    def _reset_employee_data(self):
+        """
+        Updates the "work_days" key for each employee based on the start date.
+        """
+        year, month, day = self.get_start_date()
+        new_work_days = self._initialize_work_days(year, month, day)
+        self.employees.update_many({}, {'$set': {'work_days': new_work_days}})
+
+    def get_start_date(self):
+        """
+        Returns a tuple containing the year, month, day of the start date 
+        """
+        data = self.start_date.find()[0]
+        return (data['year'], data['month'], data['day'])
+    
+    def has_start_date(self):
+        return self.start_date.count_documents({})
+
+    
     # For debugging purposes, delete later
     def print(self):
         for employee in self.employees.find():
@@ -82,5 +156,4 @@ class DatabaseHandler:
             print("Work days:")
             for work_day in employee["work_days"]:
                 print(work_day["work_date"], work_day["day_of_week"], work_day["time_in"], work_day["time_out"], work_day["regular_hours"], work_day["overtime_hours"])
-
 
