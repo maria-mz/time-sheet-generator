3b1eb4f6049978e7102d9ed0ac3f9d14da54fc7e
Maria Zaman <mariazaman2@yahoo.com>
2024-07-24 20:10:44 -0700

    add constants

---
+++ const.py
@@ -0,0 +1,13 @@
+"""App-wide Constants"""
+import datetime
+
+PAY_PERIOD_DAYS = 14
+DEFAULT_HOURS_REG = 8
+DEFAULT_HOURS_OT = 0
+DEFAULT_TIME_IN = datetime.time(hour=9) # 9 AM
+DEFAULT_TIME_OUT = datetime.time(hour=17) # 5 PM
+
+TIME_FORMAT = "%H:%M"
+DATE_FORMAT = "%Y-%m-%d"
+
+DB_NAME = "database.db"
