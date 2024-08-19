c04dab193d0764c7bed21d580b2131a31cd81e4f
Maria Zaman <mariazaman2@yahoo.com>
2024-08-19 11:29:33 -0700

    allow filtering by multiple attributes

--- gui/employees_tab/employees_table.py
+++ gui/employees_tab/employees_table.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 from PySide6.QtWidgets import (
     QWidget, 
     QAbstractItemView,
@@ -13,10 +15,19 @@ from db.db_data import Employee
 HEADER_LABELS = ["First Name", "Last Name", "Employee No", "Job Title", "Contract"]
 
 
+class FilterIndex(Enum):
+    FIRST_NAME = 0
+    LAST_NAME = 1
+    EMPLOYEE_ID = 2
+    POSITION = 3
+    CONTRACT = 4
+
+
 class EmployeesTable(QTableWidget):
     def __init__(self, parent: QWidget = None):
         super().__init__(parent)
         self.employees = []
+        self._filter_queries = [None] * len(FilterIndex)
 
         self.setColumnCount(len(HEADER_LABELS))
         self.setHorizontalHeaderLabels(HEADER_LABELS)
@@ -48,49 +59,74 @@ class EmployeesTable(QTableWidget):
                 qitem.setFlags(~Qt.ItemIsEditable)
 
                 self.setItem(row, col, qitem)
+        
+        self._filter()
 
     def filter_by_first_name(self, name: str):
-        relaxed_name = name.lower().strip()
+        normalized_name = self._normalize(name)
 
         def query(employee: Employee) -> bool:
-            return employee.first_name.lower().startswith(relaxed_name)
+            return employee.first_name.lower().startswith(normalized_name)
 
-        self._filter(query)
-    
+        self._filter_queries[FilterIndex.FIRST_NAME.value] = query
+
+        self._filter()
+
     def filter_by_last_name(self, name: str):
-        relaxed_name = name.lower().strip()
+        normalized_name = self._normalize(name)
 
         def query(employee: Employee) -> bool:
-            return employee.last_name.lower().startswith(relaxed_name)
+            return employee.last_name.lower().startswith(normalized_name)
 
-        self._filter(query)
+        self._filter_queries[FilterIndex.LAST_NAME.value] = query
+
+        self._filter()
 
     def filter_by_position(self, position: str):
-        relaxed_position = position.lower().strip()
+        normalized_position = position.lower().strip()
 
         def query(employee: Employee) -> bool:
-            return employee.position.lower().startswith(relaxed_position)
+            return employee.position.lower().startswith(normalized_position)
 
-        self._filter(query)
+        self._filter_queries[FilterIndex.POSITION.value] = query
+
+        self._filter()
+
+    def filter_by_id(self, employee_id: str):
+        normalized_id = employee_id.lower().strip()
+
+        def query(employee: Employee) -> bool:
+            return employee.employee_id.lower().startswith(normalized_id)
+
+        self._filter_queries[FilterIndex.EMPLOYEE_ID.value] = query
+
+        self._filter()
 
     def filter_by_contract(self, contract: str):
-        relaxed_contract = contract.lower().strip()
+        normalized_contract = contract.lower().strip()
 
         def query(employee: Employee) -> bool:
-            return employee.contract.lower().startswith(relaxed_contract)
+            return employee.contract.lower().startswith(normalized_contract)
 
-        self._filter(query)
+        self._filter_queries[FilterIndex.CONTRACT.value] = query
 
-    def _filter(self, query) -> None:
+        self._filter()
+
+    def _filter(self) -> None:
         self.setCurrentItem(None) # Clear the current selection
 
         for row in range(self.rowCount()):
             employee = self.get_employee_from_row(row)
 
-            if query(employee):
-                self.setRowHidden(row, False)
-            else:
-                self.setRowHidden(row, True)
+            matches_queries = all(
+                query(employee) if query is not None else True \
+                for query in self._filter_queries
+            )
+
+            self.setRowHidden(row, not matches_queries)
+
+    def _normalize(self, val: str) -> str:
+        return val.strip().lower()
 
     def get_employee_from_row(self, row: int) -> Employee:
         return self.employees[row]
