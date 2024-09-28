ec2cd2e2b5721ee1cc41f4a3e0d25093d15cb770
Maria Zaman <mariazaman2@yahoo.com>
2024-09-27 21:37:29 -0700

    update db handler tests

--- tests/test_db_handler.py
+++ tests/test_db_handler.py
@@ -2,41 +2,41 @@ import sqlite3
 import datetime
 import pytest
 
-from db.db_handler import DatabaseHandler, EmployeeAlreadyExistsError
+from db.db_handler import DatabaseHandler, DuplicateEmployeeID
 from db.db_data import PayPeriod, Employee, Shift
 
 
-TEST_DB_PATH = "tests/test_database.db"
-
-
-@pytest.fixture(name="db_handler", scope="session")
+@pytest.fixture(name="db_handler")
 def fixture_db_handler():
-    db_handler = DatabaseHandler(TEST_DB_PATH)
+    db_handler = DatabaseHandler(db=":memory:") # RAM database
 
-    # Start with fresh empty tables
-    db_handler.delete_shift_table()
-    db_handler.delete_employee_table()
-    db_handler.delete_settings_table()
-
+    # Init all tables
     db_handler.create_employee_table()
-    db_handler.create_shift_table()
     db_handler.create_settings_table()
+    db_handler.create_shift_table()
 
     yield db_handler
 
     db_handler.close()
 
+@pytest.fixture(name="employee")
+def fixture_employee():
+    employee = Employee(
+        employee_id="1",
+        first_name="Alissa",
+        last_name="Rivers",
+        position="Ornithologist",
+        contract="Full-time",
+        shifts=[]
+    )
+    return employee
 
 def test_get_pay_period_initial(db_handler: DatabaseHandler):
-    """Verify that pay period dates are null when table is created"""
-
     pay_period = db_handler.get_pay_period()
     assert pay_period is None
 
 
 def test_update_pay_period(db_handler: DatabaseHandler):
-    """Test updating the pay period"""
-
     pay_period = PayPeriod(
         start_date=datetime.date(year=2024, month=7, day=1),
         end_date=datetime.date(year=2024, month=7, day=14)
@@ -48,51 +48,33 @@ def test_update_pay_period(db_handler: DatabaseHandler):
     assert actual_pay_period == pay_period
 
 
-def test_add_employee__success(db_handler: DatabaseHandler):
-    """Test adding an employee"""
+def test_add_employee__success(db_handler: DatabaseHandler, employee: Employee):
+    db_handler.add_employee(employee)
+    assert db_handler.get_employee(employee.employee_id) == employee
 
-    employee = Employee(
-        employee_id="1",
-        full_name="Charlie Brown",
-        position="Teacher",
-        shifts=[]
-    )
 
+def test_add_employee__duplicate(db_handler: DatabaseHandler, employee: Employee):
     db_handler.add_employee(employee)
 
-    assert db_handler.get_employee("1") == employee
+    with pytest.raises(DuplicateEmployeeID):
+        db_handler.add_employee(employee)
 
 
-def test_add_employee__duplicate(db_handler: DatabaseHandler):
-    """Test adding an employee with the same employee_id"""
-
-    employee2 = Employee(
-        employee_id="1",
-        full_name="Franklin",
-        position="Architect",
-        shifts=[]
-    )
-
-    with pytest.raises(EmployeeAlreadyExistsError):
-        db_handler.add_employee(employee2)
-
-
-def test_update_employee(db_handler: DatabaseHandler):
-    """Test updating the field of an employee"""
-
-    employee = db_handler.get_employee("1")
-    employee.position = "Writer"
+def test_update_employee(db_handler: DatabaseHandler, employee: Employee):
+    db_handler.add_employee(employee)
+    employee.position = "Wildlife Biologist"
 
     db_handler.update_employee(employee)
 
     assert db_handler.get_employee(employee.employee_id) == employee
 
 
-def test_add_shifts__success(db_handler: DatabaseHandler):
+def test_add_shifts__success(db_handler: DatabaseHandler, employee: Employee):
     """
     Test adding some shifts for an existing employee.
     Also verifies that shifts are returned in ascending order.
     """
+    db_handler.add_employee(employee)
 
     shifts = [
         # Intentionally out of order
@@ -107,9 +89,9 @@ def test_add_shifts__success(db_handler: DatabaseHandler):
     ]
 
     for shift in shifts:
-        db_handler._add_shift("1", shift)
+        db_handler._add_shift(employee.employee_id, shift)
 
-    actual_shifts = db_handler._get_shifts("1")
+    actual_shifts = db_handler._get_shifts(employee.employee_id)
 
     # Handler's result should be in order
     assert actual_shifts[0] == shifts[1]
@@ -127,50 +109,60 @@ def test_add_shift__no_employee(db_handler: DatabaseHandler):
         db_handler._add_shift("5", shift)
 
 
-def test_update_shift(db_handler: DatabaseHandler):
+def test_update_shift(db_handler: DatabaseHandler, employee: Employee):
     """Test updating an existing shift"""
+
+    db_handler.add_employee(employee)
 
     shift = Shift(
         date=datetime.date(year=2024, month=7, day=20),
         time_out=datetime.time(hour=12),
         hours_reg="3.00"
     )
+    db_handler._add_shift(employee.employee_id, shift)
 
-    db_handler._update_shift("1", shift)
+    shift.hours_reg = "8.00"
 
-    actual_shift = db_handler._get_shifts("1")[0]
+    db_handler._update_shift(employee.employee_id, shift)
+
+    actual_shift = db_handler._get_shifts(employee.employee_id)[0]
     assert actual_shift == shift
 
 
-def test_update_shift__no_change(db_handler: DatabaseHandler):
+def test_update_shift__no_change(db_handler: DatabaseHandler, employee: Employee):
     """Test updating a shift that doesn't exist"""
+
+    db_handler.add_employee(employee)
+
+    shift = Shift(
+        date=datetime.date(year=2024, month=7, day=20),
+        time_out=datetime.time(hour=12),
+    )
+    db_handler._add_shift(employee.employee_id, shift)
 
     # This shift doesn't exist. Changes in update will only be made
     # to existing shifts which occur on some day
-    shift = Shift(
+    shift2 = Shift(
         date=datetime.date(year=2025, month=1, day=1),
     )
 
-    db_handler._update_shift("1", shift)
+    db_handler._update_shift(employee.employee_id, shift2)
 
-    assert len(db_handler._get_shifts("1")) == 2 # at this time should still be 2 shifts
-    # TODO: check contents of shifts are the same
-
-
-def test_delete_shift(db_handler: DatabaseHandler):
-    """Test deleting an existing shift"""
-
-    db_handler._delete_shift("1", datetime.date(year=2024, month=7, day=20))
-
-    assert len(db_handler._get_shifts("1")) == 1
+    assert len(db_handler._get_shifts(employee.employee_id)) == 1
 
 
-def test_delete_employee(db_handler: DatabaseHandler):
+def test_delete_employee(db_handler: DatabaseHandler,  employee: Employee):
     """Test deleting an employee - should delete their shifts too"""
 
-    db_handler.delete_employee("1")
+    db_handler.add_employee(employee)
 
-    assert db_handler.get_employee("1") is None
-    assert db_handler._get_shifts("1") == []
+    shift = Shift(
+        date=datetime.date(year=2024, month=7, day=20),
+        time_out=datetime.time(hour=12),
+    )
+    db_handler._add_shift(employee.employee_id, shift)
 
-# TODO: Write more tests, compare updating vs adding
+    db_handler.delete_employee(employee.employee_id)
+
+    assert db_handler.get_employee(employee.employee_id) is None
+    assert db_handler._get_shifts(employee.employee_id) == []
